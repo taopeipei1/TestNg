d5cb1d6b4c5322f6eaac5fe466031839292e7444
1916853003@qq.com <1916853003@qq.com>
2020-03-17 17:03:49 +0800

    merge1

--- Test1/src/main/java/test/TestClass.java
+++ Test1/src/main/java/test/TestClass.java
@@ -14,12 +14,11 @@ public class TestClass {
 
     }
 
-<<<<<<< HEAD
-=======
-    }
+
+
     public int[] getArray(){
-        int[] a ={1,2,3,4,5,6,7};
+        int[] a ={1,2,3,4,5,6,7,8};
         return a;
     }
->>>>>>> dev
+
 }
